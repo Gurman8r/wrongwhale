f633e55e6124241c63c08ef3733f72511b48013f
gurman8r <gurman8r@gmail.com>
2023-11-18 00:42:01 -0500

    improved world preview

--- Aster/src/ui_title_loadgame.gd
+++ Aster/src/ui_title_loadgame.gd
@@ -5,32 +5,33 @@ extends Control
 const world_preview_prefab = preload("res://assets/scenes/world_preview.tscn")
 
 @onready var preview_root = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer
+@onready var empty_label = $MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/EmptyLabel
 
 var path_list: Array[String]
+var previews: Array[WorldPreview]
 
 func _ready() -> void:
 	visibility_changed.connect(_on_visibility_changed)
-	_on_visibility_changed()
 
 func _on_visibility_changed() -> void:
-	if not visible:
-		path_list.clear()
-		for child in preview_root.get_children():
-			child.queue_free()
-	else:
-		path_list = WorldData.list()
-		for i in range(0, path_list.size()):
-			var path: String = path_list[i]
-			var world_preview: WorldPreview = world_preview_prefab.instantiate()
-			preview_root.add_child(world_preview)
-			var world_data = WorldData.read(path)
-			world_preview.label_name.text = world_data.name
-			world_preview.button_play.pressed.connect(func():
-				Ref.main.load_world_from_file(path))
-			world_preview.button_confirm_delete.pressed.connect(func():
-				var dir_path = WorldData.get_dir_path(path)
-				Util.wipe(dir_path)
-				world_preview.queue_free())
+	if not visible: clear()
+	else: refresh()
+
+func clear() -> void:
+	for preview in previews: preview.queue_free()
+	path_list.clear()
+	previews.clear()
+
+func refresh() -> void:
+	path_list = WorldData.list()
+	previews = []
+	empty_label.visible = path_list.is_empty()
+	for i in range(0, path_list.size()):
+		var path: String = path_list[i]
+		var world_preview: WorldPreview = world_preview_prefab.instantiate()
+		previews.append(world_preview)
+		preview_root.add_child(world_preview)
+		world_preview.set_world_data(WorldData.read(path))
 
 func _on_button_back_pressed() -> void:
 	Ref.ui.title.current_menu = Ref.ui.title.main
--- Aster/src/world_preview.gd
+++ Aster/src/world_preview.gd
@@ -2,24 +2,31 @@
 class_name WorldPreview
 extends PanelContainer
 
-@onready var label_name = $MarginContainer/HBoxContainer/PanelContainer/MarginContainer/LabelName
-@onready var button_play = $MarginContainer/HBoxContainer/ButtonPlay
-@onready var button_delete = $MarginContainer/HBoxContainer/ButtonDelete
+@onready var name_panel = $MarginContainer/HBoxContainer/NamePanel
+@onready var name_label = $MarginContainer/HBoxContainer/NamePanel/MarginContainer/NameLabel
+@onready var buttons_panel = $MarginContainer/HBoxContainer/ButtonsPanel
+@onready var play_button = $MarginContainer/HBoxContainer/ButtonsPanel/MarginContainer/HBoxContainer/PlayButton
+@onready var delete_button = $MarginContainer/HBoxContainer/ButtonsPanel/MarginContainer/HBoxContainer/DeleteButton
+@onready var edit_button = $MarginContainer/HBoxContainer/ButtonsPanel/MarginContainer/HBoxContainer/EditButton
+@onready var delete_confirmation_panel = $MarginContainer/HBoxContainer/DeleteConfirmationPanel
+@onready var delete_message_label = $MarginContainer/HBoxContainer/DeleteConfirmationPanel/MarginContainer/HBoxContainer/DeleteMessageLabel
+@onready var delete_confirm_button = $MarginContainer/HBoxContainer/DeleteConfirmationPanel/MarginContainer/HBoxContainer/DeleteConfirmButton
+@onready var delete_cancel_button = $MarginContainer/HBoxContainer/DeleteConfirmationPanel/MarginContainer/HBoxContainer/DeleteCancelButton
 
-@onready var label_delete_confirmation = $MarginContainer/HBoxContainer/LabelDeleteConfirmation
-@onready var button_confirm_delete = $MarginContainer/HBoxContainer/ButtonConfirmDelete
-@onready var button_cancel_delete = $MarginContainer/HBoxContainer/ButtonCancelDelete
-
-func _ready() -> void:
-	button_delete.pressed.connect(func():
-		button_play.hide()
-		button_delete.hide()
-		label_delete_confirmation.show()
-		button_confirm_delete.show()
-		button_cancel_delete.show())
-	button_cancel_delete.pressed.connect(func():
-		button_play.show()
-		button_delete.show()
-		label_delete_confirmation.hide()
-		button_confirm_delete.hide()
-		button_cancel_delete.hide())
+func set_world_data(world_data: WorldData):
+	name_label.text = world_data.name
+	play_button.pressed.connect(func():
+		Ref.main.load_world_from_file(world_data.guid))
+	delete_button.pressed.connect(func():
+		name_label.hide()
+		buttons_panel.hide()
+		delete_confirmation_panel.show()
+		delete_message_label.text = "Are you sure you want to delete %s?" % [world_data.name])
+	delete_confirm_button.pressed.connect(func():
+		Util.wipe(WorldData.get_dir_path(world_data.guid))
+		Ref.ui.title.loadgame.refresh()
+		queue_free())
+	delete_cancel_button.pressed.connect(func():
+		name_label.show()
+		buttons_panel.show()
+		delete_confirmation_panel.hide())
